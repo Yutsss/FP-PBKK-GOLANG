06279dac12af447b065bd56d2d714fa26f212d49
Yutsss <adyuta123@gmail.com>
2024-12-02 11:20:13 +0700

    cors

--- BE/middleware/cors.go
+++ BE/middleware/cors.go
@@ -3,15 +3,16 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3001")
+		clientUrl := os.Getenv("CLIENT_URL")
+		c.Header("Access-Control-Allow-Origin", clientUrl)
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
-
 		if c.Request.Method == http.MethodOptions {
 			c.AbortWithStatus(http.StatusNoContent)
 		} else {
